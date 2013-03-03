3e7f581f78cd58ea28e91fc7974e838f4e72506e
codesharper <andreashoglund@msn.com>
2013-03-03 22:18:25 +0100

    adding new tests for xml

--- src/MarkupMapper.UnitTests/Xml/XmlMarkupTests.cs
+++ src/MarkupMapper.UnitTests/Xml/XmlMarkupTests.cs
@@ -1,49 +1,68 @@
-﻿using NUnit.Framework;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Should;
 using System.Xml.Linq;
 using MarkupMapper.Xml;
 using MarkupMapper.Xml.CustomExceptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MarkupMapper.UnitTests.Xml
 {
-    [TestFixture]
+    [TestClass]
     public class XmlMarkupTests
     {
-        const string Model1Xml = "<Model1 />";
+        const string Model1Xml = "<Model1 />";        
         readonly XElement xModel1 = XElement.Parse(Model1Xml);
+        readonly XElement xModel2 = new XElement("Model2", new XAttribute("Id", 1));
 
         class Model1
         {
             
         }
 
-        [Test]
+        class Model2
+        {
+            public int Id { get; set; }
+        }
+
+        [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
         public void Map_Model1_FromNull_ThrowsArgumentNullException()
         {
             XmlMapper.Map<Model1>(null);
         }
 
-        [Test]
+        [TestMethod]
         [ExpectedException(typeof(XmlMappingException))]
-        public void Map_Model1_FromWrongRootElementName_ThrowsMappingException()
+        public void Map_Model1_FromWrongRootElementName_ThrowsXmlMappingException()
         {
             MarkupMapper.Xml.XmlMapper.Map<Model1>(new XElement("x"));
         }
-
-
-
-
-        [Test]
-        [Ignore]
-        public void Map_Model1FromXml_GetsCreated()
+        
+        [TestMethod]
+        public void Map_Model1FromModel1Xml_GetsCreated()
         {
             Model1 Model1 = XmlMapper.Map<Model1>(xModel1);
+            Assert.IsNotNull(Model1);
+        }
+
+        [TestMethod]
+        public void Map_ClassWithOneProperty_Fromth_XmlWithSameAttribute_GetsMapped()
+        {
+            Model2 model2 = (Model2)XmlMapper.Map(xModel2, typeof(Model2));
+
+            string idFromXml = xModel2.Attribute("Id").Value;
+            Assert.AreEqual(1, model2.Id);
+
+        }
+
+        [TestMethod]        
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Map_ClassWithoutParameterlessConstructor_ThrowsInvalidOperationException()
+        {
+            Assert.Fail("Fail");
         }
     }
 }
---
+++ src/MarkupMapper.Xml/XmlMapper.cs
@@ -0,0 +1,55 @@
+﻿using MarkupMapper.Xml.CustomExceptions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkupMapper.Xml
+{
+    public static class XmlMapper
+    {
+        public static T1 Map<T1>(System.Xml.Linq.XElement xElementToMapFrom)
+        {
+            var mapObj = Map(xElementToMapFrom, typeof(T1));
+            if (mapObj == null) return default(T1);
+            return (T1)mapObj;            
+        }
+
+        public static object Map(System.Xml.Linq.XElement xSource, Type sourceType)
+        {
+            if (xSource == null) throw new ArgumentNullException("xSource");
+
+            var xmlRootName = xSource.Name.LocalName;
+            
+            if (!sourceType.IsGenericType)
+            {
+                string typeName = sourceType.Name;
+
+                if (typeName != xmlRootName)
+                {
+                    throw new XmlMappingException(string.Format("xml root name='{0}' does not match Generic Type Name='{1}'", xmlRootName, typeName));
+                }
+                var instance = Activator.CreateInstance(sourceType);
+
+                // Metod MapProperties()
+                foreach (var xProperty in xSource.Attributes())
+                {
+                    var clrProperty = sourceType.GetProperty(xProperty.Name.ToString());
+                    if (clrProperty != null && clrProperty.CanWrite)
+                    {
+                        var propertyType = clrProperty.PropertyType;
+                        TypeConverter converter = TypeDescriptor.GetConverter(propertyType);
+                        clrProperty.SetValue(instance, converter.ConvertFromString(xProperty.Value));
+                    }
+
+                }
+
+                return instance;
+                
+            }
+            return null;
+        }
+    }
+}
